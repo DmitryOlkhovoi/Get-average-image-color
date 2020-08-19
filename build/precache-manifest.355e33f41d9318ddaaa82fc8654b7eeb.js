1f02dc3572cdec73b592d3c9391ce2fd0e28c553
DmitryOlkhovoi <auuine@gmail.com>
2020-08-19 19:47:00 +0300

    add demo

---
+++ build/precache-manifest.355e33f41d9318ddaaa82fc8654b7eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a014d92f536b33a9ed00ab91c7d18f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075f106c1e3f8adedac9",
+    "url": "/static/css/main.c8b73f86.chunk.css"
+  },
+  {
+    "revision": "391f7531d9f0bdc07ab3",
+    "url": "/static/js/2.5ef7cab4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5ef7cab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075f106c1e3f8adedac9",
+    "url": "/static/js/main.3dae33e4.chunk.js"
+  },
+  {
+    "revision": "d642d9d740bca52f6fbe",
+    "url": "/static/js/runtime-main.1fc0ba4b.js"
+  }
+]);
